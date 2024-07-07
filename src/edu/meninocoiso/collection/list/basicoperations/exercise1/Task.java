35ecbf0cdbaa982314045b072856d7775904062b
Eduardo <eduardomacielbr@gmail.com>
2024-07-07 00:49:14 -0300

    feat: basic list operations exercises

---
+++ src/edu/meninocoiso/collection/list/basicoperations/exercise1/Task.java
@@ -0,0 +1,13 @@
+package edu.meninocoiso.collection.list.basicoperations.exercise1;
+
+public class Task {
+	private String title;
+
+	public Task(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+}
---
+++ src/edu/meninocoiso/collection/list/basicoperations/exercise1/TasksList.java
@@ -0,0 +1,71 @@
+package edu.meninocoiso.collection.list.basicoperations.exercise1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TasksList {
+	private List<Task> tasks;
+
+	public TasksList() {
+		tasks = new ArrayList<>();
+	}
+
+	public void addTask(String title) {
+		tasks.add(new Task(title));
+	}
+
+	public void removeTask(String title) {
+		if (tasks.isEmpty()) {
+			System.out.println("A lista se encontra vazia!");
+			return;
+		}
+
+		tasks.removeIf(task -> task.getTitle().equalsIgnoreCase(title));
+		/* ou:
+		for (Task task : tasks) {
+			if (task.getTitle().equals(title)) {
+				tasks.remove(task);
+			}
+		}*/
+	}
+
+	public int getTasksLength() {
+		return tasks.size();
+	}
+
+	public String[] getTasksTitles() {
+		String[] titles = new String[tasks.size()];
+		for (int i = 0; i < tasks.size(); i++) {
+			titles[i] = tasks.get(i).getTitle();
+		}
+		return titles;
+	}
+
+	@Override
+	public String toString() {
+		return "TasksList{" +
+				"tasks=" + tasks +
+				'}';
+	}
+
+	public static void main(String[] args) {
+		TasksList tasksList = new TasksList();
+		System.out.println(tasksList.toString());
+
+		tasksList.addTask("Task 1");
+		tasksList.addTask("Task 2");
+		tasksList.addTask("Task 2");
+		tasksList.addTask("Task 3");
+
+		System.out.println("A lista de tarefas possui " + tasksList.getTasksLength() + " tarefas.");
+
+		System.out.println("Títulos das tarefas:");
+		for (String title : tasksList.getTasksTitles()) {
+			System.out.println(title);
+		}
+
+		tasksList.removeTask("Task 2");
+
+		System.out.println("A lista de tarefas possui " + tasksList.getTasksLength() + " tarefas.");
+	}
+}
